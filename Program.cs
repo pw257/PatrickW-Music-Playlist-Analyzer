77d85d69c45cfa3692b38cd906db78cfbcbf484a
pw257 <42645110+pw257@users.noreply.github.com>
2018-12-07 20:04:57 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlaylistData
+{
+    class Program
+    {
+        public int Name { get; set; }
+
+        public int Artist { get; set; }
+
+        public int Album { get; set; }
+
+        public int Genre { get; set; }
+
+        public int Size { get; set; }
+
+        public int Time { get; set; }
+
+        public int Year { get; set; }
+
+        public int Plays { get; set; }
+    }
+}
